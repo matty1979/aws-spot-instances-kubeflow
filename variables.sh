15cd49b5aaf3ddd482170271d609549363d65f6a
matty1979 <matthew.king@octoconsulting.com>
2021-03-30 12:22:14 -0400

    change to us-east-2

--- variables.sh
+++ variables.sh
@@ -8,7 +8,7 @@ export CONFIG_URI="https://raw.githubusercontent.com/kubeflow/manifests/v1.0.1/k
 # For example, your deployment name can be 'my-kubeflow' or 'kf-test'.
 # This name should match the kubeflow cluster name that can be found under envs/$ENVIRONMENT/cluster-spec.yml > metadata.name
 # If the name doesn't match, you will face issues with issuing an ELB.
-export KF_NAME=kubeflow-us-east-1
+export KF_NAME=kubeflow-us-east-2
 
 # Set the path to the base directory where you want to store one or more 
 # Kubeflow deployments. For example, /opt.
